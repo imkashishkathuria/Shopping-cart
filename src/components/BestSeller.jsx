64baf419a4bf8fd63e75feea0632ef58e75ec9e1
Kashish <kathuriak805@gmail.com>
2024-10-26 19:27:06 +0530

    displayed best sellers products on home page

---
+++ src/components/BestSeller.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from "react"
+import { ShopContext } from "../context/ShopContext"
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+    const {products} = useContext(ShopContext);
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(()=>{
+        const bestProduct = products.filter((item)=>(item.bestseller == false));
+        setBestSeller(bestProduct.slice(-5));
+    },[])
+
+  return (
+    <div className="my-10">
+        <div className="text-center text-3xl py-8">
+            <Title text1={"BEST"} text2={"SELLERS"} />
+            <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium nulla deleniti quidem ullam perspiciatis sunt itaque modi molestiae minus doloribus.
+            </p>
+
+        </div>
+        {/* Rendering Products */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+            {bestSeller.map((item, index)=>(
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+            ))}
+        </div>
+      
+    </div>
+  )
+}
+
+export default BestSeller
