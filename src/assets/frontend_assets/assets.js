fffdce74ef6b3c31f194f560d9027b064d5aaffc
Kashish <kathuriak805@gmail.com>
2024-10-26 14:05:49 +0530

    completed hero section

--- src/assets/frontend_assets/assets.js
+++ src/assets/frontend_assets/assets.js
@@ -73,10 +73,12 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
+import hero3_img from './hero3_img.png'
 
 export const assets = {
     logo,
     hero_img,
+    hero3_img,
     cart_icon,
     dropdown_icon,
     exchange_icon,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,34 @@
+import { assets } from "../assets/frontend_assets/assets"
 
 
 const Hero = () => {
   return (
-    <div>
-      <h1>
-        Hero Section 
-      </h1>
+    <div className="flex flex-col sm:flex-row border border-gray-500">
+       {/* Hero left side  */}
+       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
+        <div className="text-[#414141]">
+            <div className="flex items-center gap-2">
+                <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+                <p className="font-medium text-sm md:text-base">
+                    Our Bestsellers
+                </p>
+            </div>
+            <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+                Latest Arrivals
+            </h1>
+            <div className="flex items-center gap-2">
+                <p className="font-semibold text-sm md:text-base">
+                    Shop Now
+                </p>
+                <p className="w-8 md:w-11 h-[1px] bg-[#414141]">
+
+                </p>
+            </div>
+        </div>
+       </div>
+
+       {/* Hero right side  */}
+       <img className="w-full sm:w-1/2" src={assets.hero3_img} alt="" />
     </div>
   )
 }
